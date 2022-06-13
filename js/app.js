6161ed3668be912371bbbb075a2f2eb0cbc99c02
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-12 23:47:42 -0500

    separate buttons and info, make house more expensive relative to woodOverTime

--- js/app.js
+++ js/app.js
@@ -8,34 +8,42 @@ const villagerCount = document.getElementById('villager-count')
 wood.addEventListener('click', addWood)
 house.addEventListener('click', addHouse)
 
+
 let woodTotal = 0
 let woodOverTime = 0
 let houseTotal = 0
 let housesOverTime = 0
 let villagers = 1
 
+
 function render() {
   woodCount.innerText = woodTotal + ' wood'
   houseCount.innerText = houseTotal + ' houses'
   villagerCount.innerText = villagers + ' villagers'
+  console.log(woodTotal)
+  console.log(woodOverTime)
+  console.log(houseTotal)
+  if(woodOverTime > 100){
+    house.innerText = 'Build House - 50 wood'}
 }
 
 function addWood() {
-  woodTotal++
-  woodOverTime++ 
+  woodTotal = woodTotal + villagers
+  woodOverTime = woodOverTime + villagers
   render()
 }
 
-
-
 function addHouse() {
   if(woodTotal >= 25) {
-    houseTotal++
-    housesOverTime++
     woodTotal = woodTotal - 25
+  } else if (woodTotal > 100){
+    woodTotal= woodTotal - 50
   } else {
     return
   }
+  villagers = villagers + 3
+  houseTotal++
+  housesOverTime++
   render()
 }
 
